b11683830cefd366c6a2bcfe7dbcf5383007b015
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-10-18 16:14:40 +0300

    customer service

--- customer/src/main/java/com/wycliffe/customer/Customer.java
+++ customer/src/main/java/com/wycliffe/customer/Customer.java
@@ -1,11 +1,27 @@
 package com.wycliffe.customer;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
+import javax.persistence.*;
+
+@AllArgsConstructor
 @Builder
+@Data
+@Entity
+@NoArgsConstructor
 public class Customer {
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
     private Integer id;
     private String firstname;
     private String lastname;
--- customer/src/main/java/com/wycliffe/customer/CustomerService.java
+++ customer/src/main/java/com/wycliffe/customer/CustomerService.java
@@ -1,12 +1,30 @@
 package com.wycliffe.customer;
 
-public record CustomerService() {
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstname(request.firstname())
                 .lastname(request.lastname())
                 .email(request.email())
                 .build();
-
+        customerRepository.save(customer);
+//        //todo: check if email valid
+//        //todo: check if email not taken
+//        customerRepository.saveAndFlush(customer);
+//        //todo: check if fraudster
+//        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+//                "http://FRAUD/api/v1/fraud-check/{customerId}",
+//                FraudCheckResponse.class,
+//                customer.getId()
+//        );
+//        assert fraudCheckResponse != null;
+//        if (fraudCheckResponse.isFraudster()) {
+//            throw new IllegalStateException("fraudster");
+//        }
+//        //todo: send notification
     }
 }
